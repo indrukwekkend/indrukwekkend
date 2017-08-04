b19194ad51a3ffdebfde93a79ed522f804000b23
odil-io <github@odil.io>
2017-08-04 14:42:21 +0200

    Remove gizburdt/cuztom dependency

--- plugin/ndrkwkknd-posttypes/ndrkwkknd-posttypes.php
+++ plugin/ndrkwkknd-posttypes/ndrkwkknd-posttypes.php
@@ -4,13 +4,7 @@ Plugin Name: Site Plugin for indrukwekkend.nl
 Description: Add various Custom Post Types.
 */
 
-include( 'cuztom/cuztom.php' );
 
-function content_types()
-{
-    $book = register_cuztom_post_type( "Brand", array(), array() );
-}
-add_action( 'init', 'content_types' );
 
 
 ?>
