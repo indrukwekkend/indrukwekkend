666a8148d42a1a8f954216c9972f2f488bf166c7
odilio <github@odil.io>
2017-08-21 11:13:09 +0200

    Update template

--- templates/content-single-teamleden.php
+++ templates/content-single-teamleden.php
@@ -1,41 +1,54 @@
 <?php while (have_posts()) : the_post(); ?>
-<?php get_template_part('templates/teamleden/single', 'intro'); ?>
 
-<?php // Teamlid Contact?>
-<?php get_template_part('templates/teamleden/teamleden', 'contact'); ?>
+	<?php // Intro ?>
+	<?php get_template_part('templates/teamleden/single', 'intro'); ?>
 
+	<?php // Contact ?>
+	<?php get_template_part('templates/teamleden/teamleden', 'contact'); ?>
 
-<?php // Content ?>
-<?php if( have_rows('content') ): ?>
-<div class="row single-content">
-<div class="container">
-<?php while( have_rows('content') ): the_row(); ?>
+	<?php // Content ?>
+	<?php if( have_rows('content') ): ?>
 
-<?php if( get_row_layout() == 'image_left' ): ?>
-<?php // Align image to left ?>
-<?php get_template_part('templates/parts/image', 'left'); ?>
+		<div class="row single-content">
+			<div class="container">
 
-<?php elseif(get_row_layout() == 'image_right' ): ?>
-<?php // Align image to right ?>
-<?php get_template_part('templates/parts/image', 'right'); ?>
+				<?php while( have_rows('content') ): the_row(); ?>
 
-<?php elseif(get_row_layout() == 'image_fullwidth' ): ?>
-<?php // Span image over container ?>
-<?php get_template_part('templates/parts/image', 'fullwidth'); ?>
+					<?php if( get_row_layout() == 'image_left' ): ?>
+						<?php // Align image to left ?>
+						<?php get_template_part('templates/parts/image', 'left'); ?>
 
-<?php elseif(get_row_layout() == 'image_multi' ): ?>
-<?php // Span image over container ?>
-<?php get_template_part('templates/parts/image', 'multi'); ?>
+					<?php elseif(get_row_layout() == 'image_right' ): ?>
+						<?php // Align image to right ?>
+						<?php get_template_part('templates/parts/image', 'right'); ?>
 
-<?php endif; ?>
+					<?php elseif(get_row_layout() == 'image_fullwidth' ): ?>
+						<?php // Span image over container ?>
+						<?php get_template_part('templates/parts/image', 'fullwidth'); ?>
 
-<?php endwhile; ?>
+					<?php elseif(get_row_layout() == 'image_multi' ): ?>
+						<?php // Span image over container ?>
+						<?php get_template_part('templates/parts/image', 'multi'); ?>
 
-</div>
-</div>
+					<?php endif; ?>
 
-<?php // Social Media ?>
-<?php get_template_part('templates/teamleden/teamleden', 'socialmedia'); ?>
+				<?php endwhile; ?>
 
-<?php endif; ?>
+			</div>
+		</div>
+
+		<?php // Social Media ?>
+		<?php get_template_part('templates/teamleden/teamleden', 'socialmedia'); ?>
+
+		<?php // Wat doet [teamlid] verder? ?>
+		<?php get_template_part('templates/teamleden/teamleden', 'extra'); ?>
+
+		<?php // Cases ?>
+		<?php get_template_part('templates/teamleden/teamleden', 'cases'); ?>
+
+		<?php // Andere teamleden ?>
+		<?php get_template_part('templates/teamleden/teamleden', 'teamleden'); ?>
+
+	<?php endif; ?>
+
 <?php endwhile; ?>
