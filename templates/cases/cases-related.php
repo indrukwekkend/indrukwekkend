222b9e310cd18eba84b99e9be1cba8a67f7d727e
odilio <github@odil.io>
2017-08-22 14:45:26 +0200

    Replace post objects with ACF fields

--- templates/cases/cases-related.php
+++ templates/cases/cases-related.php
@@ -1,39 +1,27 @@
-<div class="row related">
-<div class="container-fluid">
-<div class="row align-items-center">
-<div class="col-3 text-right pr-5">
-<span class="related-title"> Bekijk de<br> andere singles
-</span>
-</div>
-<?php if ( $tags = wp_get_post_tags($post->ID) ) : ?>
-<?php $tag_ids = array(); ?>
-<?php foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id; ?>
-<?php
-$args = array(
-'post_type' => 'cases',
-'tag__in' => $tag_ids,
-'post__not_in' => array($post->ID),
-'posts_per_page' => 3,
-'caller_get_posts' => 1
-);
-?>
-<?php $my_query = new wp_query( $args ); ?>
-<?php while( $my_query->have_posts() ) : ?>
-<?php $my_query->the_post(); ?>
-<div class="col-3 related-item">
-<div class="related-content">
-<div class="related-image">
-<img class="img-fluid" src="<?php the_field('header_foreground'); ?>" />
-</div>
-<div class="related-title">
-<?php the_title(); ?>
-</div>
-</div>
-</div>
-<?php endwhile; ?>
-<?php wp_reset_postdata(); ?>
-<?php wp_reset_query(); ?>
+<?php if( $post_objects = get_field('cases') ): ?>
+	<div class="row related">
+		<div class="container-fluid">
+			<div class="row align-items-center">
+				<div class="col-3 text-right pr-5">
+					<span class="related-title"> Bekijk de<br> andere cases
+					</span>
+				</div>
+				<div class="col-9 p-0">
+					<div class="related-list">
+						<?php foreach( $post_objects as $post): ?>
+							<div class="col related-item">
+								<a class="related-content" href="<?php echo esc_url( get_post_permalink() ); ?>">
+									<div class="related-image" style="background-image:url(<?php the_field('header_foreground');?>);"></div>
+									<div class="related-title">
+										<?php the_title(); ?>
+									</div>
+								</a>
+							</div>
+						<?php endforeach; ?>
+						<?php wp_reset_postdata(); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 <?php endif; ?>
-</div>
-</div>
-</div>
