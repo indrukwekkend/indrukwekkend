c2b2376202faa0f74be1669d01daa21040e4c20e
odilio <github@odil.io>
2017-08-22 14:44:40 +0200

    Update template to use /cases/single-intro part

--- templates/content-single-cases.php
+++ templates/content-single-cases.php
@@ -1,43 +1,49 @@
 <?php while (have_posts()) : the_post(); ?>
-<?php get_template_part('templates/parts/single', 'intro'); ?>
+	<?php get_template_part('templates/cases/single', 'intro'); ?>
 
-<?php // Content ?>
-<?php if( have_rows('content') ): ?>
-<div class="row single-content">
-<div class="container">
-<?php while( have_rows('content') ): the_row(); ?>
-<?php if( get_row_layout() == 'team_quote_left' ): ?>
-<?php // Quote with autograph?>
-<?php get_template_part('templates/parts/quote', 'team-left'); ?>
+	<?php // Content ?>
+	<?php if( have_rows('content') ): ?>
+		<div class="row single-content">
+			<div class="container">
+				<?php while( have_rows('content') ): the_row(); ?>
 
-<?php elseif( get_row_layout() == 'client_quote_left' ): ?>
-<?php // Quote of Client + Name, Company/Position ?>
-<?php get_template_part('templates/parts/quote', 'left'); ?>
 
-<?php elseif( get_row_layout() == 'image_left' ): ?>
-<?php // Title left, Image underneat, text right ?>
-<?php get_template_part('templates/parts/image', 'left'); ?>
+					<?php // Quote with autograph?>
+					<?php if( get_row_layout() == 'team_quote_left' ): ?>
+						<?php get_template_part('templates/parts/quote', 'team-left'); ?>
 
-<?php elseif(get_row_layout() == 'image_right' ): ?>		
-<?php // Title right, Image underneat, text left ?>
-<?php get_template_part('templates/parts/image', 'right'); ?>
+						<?php // Quote of Client + Name, Company/Position ?>
+						<?php elseif( get_row_layout() == 'client_quote_left' ): ?>
+							<?php get_template_part('templates/parts/quote', 'left'); ?>
 
-<?php elseif(get_row_layout() == 'image_fullwidth' ): ?>
-<?php get_template_part('templates/parts/image', 'fullwidth'); ?>
-<?php endif; ?>
-<?php endwhile; ?>
-<?php // Teamleden ?>
-<?php get_template_part('templates/cases/cases', 'teamleden'); ?>
-</div>
-</div>
-<?php endif; ?>
+						<?php // Title left, Image underneat Title, text right ?>
+						<?php elseif( get_row_layout() == 'image_left' ): ?>
+							<?php get_template_part('templates/parts/image', 'left'); ?>
 
-<?php // Client Quote ?>	
-<?php get_template_part('templates/cases/cases', 'client-quote'); ?>
+						<?php // Title right, Image underneat Title, text left ?>
+						<?php elseif(get_row_layout() == 'image_right' ): ?>		
+							<?php get_template_part('templates/parts/image', 'right'); ?>
 
-<?php // Cases Quote ?>
-<?php get_template_part('templates/cases/cases', 'manager-quote'); ?>
+						<?php // Fullwidth image?>
+						<?php elseif(get_row_layout() == 'image_fullwidth' ): ?>
+							<?php get_template_part('templates/parts/image', 'fullwidth'); ?>
 
-<?php // Related singles ?>
-<?php get_template_part('templates/cases/cases', 'related'); ?>
-<?php endwhile; ?>
+					<?php endif; ?>
+				<?php endwhile; ?>
+
+				<?php // Teamleden ?>
+				<?php get_template_part('templates/cases/cases', 'teamleden'); ?>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<?php // Client Quote ?>	
+	<?php get_template_part('templates/cases/cases', 'client-quote'); ?>
+
+	<?php // Cases Quote ?>
+	<?php get_template_part('templates/cases/cases', 'manager-quote'); ?>
+
+	<?php // Related singles ?>
+	<?php get_template_part('templates/cases/cases', 'related'); ?>
+
+<?php endwhile; ?>  
