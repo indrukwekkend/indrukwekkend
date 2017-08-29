d320f490d8d2f50a8336829135a8a946a7fdec45
odilio <github@odil.io>
2017-08-29 16:05:59 +0200

    Update teamleden template

--- templates/parts/employee-cases.php
+++ templates/parts/employee-cases.php
@@ -1,24 +1,38 @@
-<?php if( $post_objects = get_field('cases') ): ?>
-			<div class="row align-items-center related-cases">
-				<div class="col-3 text-right pr-5">
-					<span class="related-title">
-						<h5>Bekijk de<br> andere cases</h5>
-					</span>
-				</div>
-				<div class="col-9 p-0">
-					<div class="related-list">
-						<?php foreach( $post_objects as $post): ?>
-							<div class="col related-item">
-								<a class="related-item-content" href="<?php echo esc_url( get_post_permalink() ); ?>">
-									<div class="related-item-image" style="background-image:url(<?php the_field('header_foreground');?>);"></div>
-									<div class="related-item-title">
-										<h4><?php the_title(); ?></h4>
-									</div>
-								</a>
-							</div>
-						<?php endforeach; ?>
-						<?php wp_reset_postdata(); ?>
+<div class="row justify-content-center pb-5 employee-cases">
+	<div class="col-lg-6 text-right">
+
+		<div class="row">
+		<?php if( $post_objects = get_field('cases') ): ?>
+			<?php foreach( $post_objects as $post): ?>
+
+				<?php setup_postdata($post); ?>
+
+				<div class="col-3 text-center case" href="<?php echo esc_url( get_permalink() ); ?>">
+					<div class="row">
+						<div class="col-12">
+							<img class="img-fluid" src="<?php the_field('header_foreground');?>">
+						</div>
+					</div>
+
+
+					<div class="row">
+						<div class="col-12">
+							<?php the_title(); ?>
+						</div>
 					</div>
 				</div>
-			</div>
-<?php endif; ?>
+
+			<?php endforeach; ?>
+			<?php wp_reset_postdata(); ?>
+		<?php endif; ?>
+		</div>
+	</div>
+	<div class="col-lg-6 text-right">
+		<div class="heading">
+			<h4 class="title">
+				<span><?php echo preg_replace('~((\w+\s){2})~', '$1' . "<br>", get_field('cases_title')); ?></span>
+			</h4>
+			<span class="line"></span>
+		</div>
+	</div>
+</div>
