07e159f7404d28368c7356e19a1eb008d002e533
odilio <github@odil.io>
2017-08-29 14:16:23 +0200

    Add part 'content-cases'

---
+++ templates/parts/content-cases.php
@@ -0,0 +1,24 @@
+<?php if( $post_objects = get_field('cases') ): ?>
+			<div class="row align-items-center related-cases">
+				<div class="col-3 text-right pr-5">
+					<span class="related-title">
+						<h5><?php echo preg_replace('~((\w+\s){2})~', '$1' . "<br>", get_field('cases_title')); ?></h5>
+					</span>
+				</div>
+				<div class="col-9 p-0">
+					<div class="related-list">
+						<?php foreach( $post_objects as $post): ?>
+							<div class="col related-item">
+								<a class="related-item-content" href="<?php echo esc_url( get_post_permalink() ); ?>">
+									<div class="related-item-image" style="background-image:url(<?php the_field('header_foreground');?>);"></div>
+									<div class="related-item-title">
+										<h4><?php the_title(); ?></h4>
+									</div>
+								</a>
+							</div>
+						<?php endforeach; ?>
+						<?php wp_reset_postdata(); ?>
+					</div>
+				</div>
+			</div>
+<?php endif; ?>
