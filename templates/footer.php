17229ca0854fa29d04f395f1d10bdb1c4fbacea8
odilio <github@odil.io>
2017-08-29 16:48:43 +0200

    Add a container around the footer content

--- templates/footer.php
+++ templates/footer.php
@@ -1,67 +1,71 @@
 <footer class="container-fluid">
-	<div class="row align-items-center py-5">
+	<div class="row">
+		<div class="container">
+			<div class="row align-items-center py-5">
 
-		<div class="col-sm-12 col-xl-3 col-md-6 mt-4 mt-xl-0 contact">
-			<div class="row align-items-center">
-				<div class="col-2">
-					<div class="icon call-us-gray"></div>
-				</div>
-				<div class="col-10">
-					<div class="row">
-						<div class="col-12"><p>De beller is sneller</p></div>
+				<div class="col-sm-12 col-xl-3 col-md-6 mt-4 mt-xl-0 contact">
+					<div class="row align-items-center">
+						<div class="col-2">
+							<div class="icon call-us-gray"></div>
+						</div>
+						<div class="col-10">
+							<div class="row">
+								<div class="col-12"><p>De beller is sneller</p></div>
+							</div>
+							<div class="row">
+								<div class="col-12"><p><strong>072 - 562 54 82</strong></p></div>
+							</div>
+						</div>
 					</div>
-					<div class="row">
-						<div class="col-12"><p><strong>072 - 562 54 82</strong></p></div>
+				</div>
+
+				<div class="col-sm-12 col-xl-3 col-md-6 mt-4 mt-xl-0 contact">
+					<div class="row align-items-center">
+						<div class="col-2">
+							<div class="icon mail-us-gray"></div>
+						</div>
+						<div class="col-10">
+							<div class="row">
+								<div class="col-12"><p>Mailen mag ook</p></div>
+							</div>
+							<div class="row">
+								<div class="col-12"><p><strong>info@indrukwekkend.nl</strong></p></div>
+							</div>
+							</div>
 					</div>
 				</div>
+
+				<div class="col-sm-12 col-xl-3 col-md-6 mt-4 mt-xl-0 contact">
+					<div class="row align-items-center">
+						<div class="col-2">
+							<div class="icon visit-us-gray"></div>
+						</div>
+						<div class="col-10">
+							<div class="row">
+								<div class="col-12"><p>Hier vind je ons</p></div>
+							</div>
+							<div class="row">
+								<div class="col-12"><p><strong>De Droogmakerij 20f, 1851 LX Heiloo</strong></p></div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-12 col-xl-3 col-md-6 mt-4 mt-xl-0">
+					<div class="row align-items-center">
+						<div class="col text-right">
+							<span>Wij zijn sociaal</span>
+						</div>
+						<div class="col">
+							<a href="#instagram"><span class="icon instagram-gray"></span></a>
+							<a href="#facebook"><span class="icon facebook-gray"></span></a>
+							<a href="#twitter"><span class="icon twitter-gray"></span></a>
+							<a href="linkedin"><span class="icon linkedin-gray"></span></a>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</div>
-
-		<div class="col-sm-12 col-xl-3 col-md-6 mt-4 mt-xl-0 contact">
-			<div class="row align-items-center">
-				<div class="col-2">
-					<div class="icon mail-us-gray"></div>
-				</div>
-				<div class="col-10">
-					<div class="row">
-						<div class="col-12"><p>Mailen mag ook</p></div>
-					</div>
-					<div class="row">
-						<div class="col-12"><p><strong>info@indrukwekkend.nl</strong></p></div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="col-sm-12 col-xl-3 col-md-6 mt-4 mt-xl-0 contact">
-			<div class="row align-items-center">
-				<div class="col-2">
-					<div class="icon visit-us-gray"></div>
-				</div>
-				<div class="col-10">
-					<div class="row">
-						<div class="col-12"><p>Hier vind je ons</p></div>
-					</div>
-					<div class="row">
-						<div class="col-12"><p><strong>De Droogmakerij 20f, 1851 LX Heiloo</strong></p></div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="col-sm-12 col-xl-3 col-md-6 mt-4 mt-xl-0">
-			<div class="row align-items-center">
-				<div class="col text-right">
-					<span>Wij zijn sociaal</span>
-				</div>
-				<div class="col">
-					<a href="#instagram"><span class="icon instagram-gray"></span></a>
-					<a href="#facebook"><span class="icon facebook-gray"></span></a>
-					<a href="#twitter"><span class="icon twitter-gray"></span></a>
-					<a href="linkedin"><span class="icon linkedin-gray"></span></a>
-				</div>
-			</div>
-		</div>
-
 	</div>
 </footer>
