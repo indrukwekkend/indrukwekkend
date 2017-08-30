c22e93536635b49b9fc7bcb790f8b604c23b9c3b
odilio <github@odil.io>
2017-08-30 14:09:41 +0200

    Update client quote template

--- templates/parts/quote-client.php
+++ templates/parts/quote-client.php
@@ -5,7 +5,7 @@
 	<?php $post = $post_object; ?>
 	<?php setup_postdata( $post ); ?>
 
-	<div class="row py-5">
+	<div class="row pt-6">
 		<div class="col-10">
 			<div class="row p-5 part-quote-client">
 				<div class="col-12">
@@ -13,7 +13,9 @@
 						<?php the_content();?>
 					</div>
 					<div class="row">
-						<div><?php the_title(); ?> <?php the_field('company_position');?></div>
+						<div class="col-12">
+							<span class="text-gray"><?php the_title(); ?> - <?php the_field('company_position');?></span>
+						</div>
 					</div>
 				</div>
 			</div>
