2b97bbf7075629e15b57429d252a5e43c6f83154
odilio <github@odil.io>
2017-08-31 15:33:18 +0200

    Add hover effect on coworkers
    Resolves: #17

--- templates/parts/employee-coworkers.php
+++ templates/parts/employee-coworkers.php
@@ -13,13 +13,33 @@
 					<div class="coworkers-list">
 						<?php while( $query->have_posts() ) : ?>
 							<?php $query->the_post(); ?>
-								<div class="col coworkers-item">
-									<a class="coworkers-content" href="<?php echo esc_url( get_post_permalink() ); ?>">
+								<div class="col-4 coworkers-item">
+									<div class="coworkers-content">
 										<div class="coworkers-image" style="background-image:url(<?php the_post_thumbnail_url();?>);"></div>
 										<div class="coworkers-title">
 											<h4><?php the_title(); ?></h4>
 										</div>
-									</a>
+										<div class="coworkers-hover">
+											<div class="coworkers-info">
+												<h4><a class="text-white" href="<?php echo esc_url( get_post_permalink() ); ?>"><?php the_title(); ?></a></h4>
+												<span><?php the_field('company_position'); ?></span>
+											</div>
+											<div class="coworkers-contact">
+												<div class="coworkers-phone">
+													<div class="mb-2 px-4">
+														<span class="icon smartphone"></span>
+														<a class="ml-2 text-white" href="tel:<?php the_field('mobile'); ?>"><?php the_field('mobile'); ?></a>
+													</div>
+												</div>
+												<div class="coworkers-email">
+													<div class="mb-2 px-4">
+														<span class="icon mail"></span>
+														<a class="ml-2 text-white" href="mailto:<?php the_field('email'); ?>"><?php the_field('email'); ?></a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
 								</div>
 						<?php endwhile; ?>
 						<?php wp_reset_postdata(); ?>
