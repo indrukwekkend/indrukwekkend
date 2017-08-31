9bdc8a3c85819acbaba432d8b95c949246059907
odilio <github@odil.io>
2017-08-31 10:49:20 +0200

    Revert back to svg autographs
    Resolves: #22

--- templates/parts/quote-left.php
+++ templates/parts/quote-left.php
@@ -1,11 +1,11 @@
 <div class="row">
 	<div class="col-lg-5 p-0">
-		<div class="part-quote p-4 p-md-5 mb-5">
+		<div class="part-quote pt-4 px-4 pb-3 pt-md-5 px-md-5 pb-md-4 mb-5">
 			<div class="quote-content">
 				<?php the_sub_field('content')?>
 			</div>
 			<div class="quote-autograph">
-				<?php the_sub_field('author')?>
+				<img height="120" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/autographs/<?php the_field('autograph', get_sub_field('author'));?>"/>
 			</div>
 		</div>
 	</div>
