669f312437a08afed06158bacb6d5e083c8beebe
odilio <github@odil.io>
2017-09-05 11:54:28 +0200

    Fix spacing issue in 'header-page.php'

--- templates/parts/heading-page.php
+++ templates/parts/heading-page.php
@@ -1,5 +1,5 @@
 <div class="row page-header">
-	<div class="container py-5">
+	<div class="container py-6">
 		<div class="row justify-content-center">
 			<div class="col-8">
 				<?php the_content(); ?>
