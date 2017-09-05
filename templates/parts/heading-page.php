b2d3363546f3679f59cc2725916fa3b13119ea1d
odilio <github@odil.io>
2017-09-05 12:12:24 +0200

    Add conditional formatting

--- templates/parts/heading-page.php
+++ templates/parts/heading-page.php
@@ -1,8 +1,15 @@
 <div class="row page-header">
+	<?php if(is_singular('teamleden')):?>
+	<div class="container pt-6">
+	<?php else:?>
 	<div class="container py-6">
+	<?php endif;?>
 		<div class="row justify-content-center">
 			<div class="col-8">
 				<?php the_content(); ?>
+				<?php if(is_singular('teamleden') && get_field('autograph')):?>
+					<img height="200" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/autographs/<?php the_field('autograph');?>"/>
+				<?php endif;?>
 			</div>
 		</div>
 	</div>
