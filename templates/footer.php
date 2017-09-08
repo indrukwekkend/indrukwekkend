d0743e611d308452639a30f1c3b21ee756eefcf9
odil-io <github@odil.io>
2017-09-08 15:14:28 +0200

    Fix footer alignment issues

--- templates/footer.php
+++ templates/footer.php
@@ -10,10 +10,10 @@
 						</div>
 						<div class="col-10">
 							<div class="row">
-								<div class="col-12"><p>De beller is sneller</p></div>
+								<div class="col-12"><p>De beller is sneller <strong class="d-inline d-md-none">072 - 562 54 82</strong></p></div>
 							</div>
 							<div class="row">
-								<div class="col-12"><p><strong>072 - 562 54 82</strong></p></div>
+								<div class="col-12"><p><strong class="d-none d-md-inline">072 - 562 54 82</strong></p></div>
 							</div>
 						</div>
 					</div>
@@ -26,10 +26,10 @@
 						</div>
 						<div class="col-10">
 							<div class="row">
-								<div class="col-12"><p>Mailen mag ook</p></div>
+								<div class="col-12"><p>Mailen mag ook <strong class="d-inline d-md-none">info@indrukwekkend.nl</strong></p></div>
 							</div>
 							<div class="row">
-								<div class="col-12"><p><strong>info@indrukwekkend.nl</strong></p></div>
+								<div class="col-12"><p><strong class="d-none d-md-inline">info@indrukwekkend.nl</strong></p></div>
 							</div>
 							</div>
 					</div>
@@ -42,10 +42,10 @@
 						</div>
 						<div class="col-10">
 							<div class="row">
-								<div class="col-12"><p>Hier vind je ons</p></div>
+								<div class="col-12"><p>Hier vind je ons <strong class="d-inline d-md-none">De Droogmakerij 20f, 1851 LX Heiloo</strong></p></div>
 							</div>
 							<div class="row">
-								<div class="col-12"><p><strong>De Droogmakerij 20f, 1851 LX Heiloo</strong></p></div>
+								<div class="col-12"><p><strong class="d-none d-md-inline">De Droogmakerij 20f, 1851 LX Heiloo</strong></p></div>
 							</div>
 						</div>
 					</div>
@@ -53,7 +53,7 @@
 
 				<div class="col-sm-8 col-xl-3 col-md-6 mt-5 mt-xl-0">
 					<div class="row align-items-center">
-						<div class="col text-xl-right">
+						<div class="col text-center text-xl-right">
 							<span>Wij zijn sociaal</span>
 							<a href="<?php the_field('instagram_link', 'option'); ?>"><span class="icon instagram-gray"></span></a>
 							<a href="<?php the_field('facebook_link', 'option'); ?>"><span class="icon facebook-gray"></span></a>
