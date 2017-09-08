b72fa3594000170649404fdbc989975129c72a79
odil-io <github@odil.io>
2017-09-08 15:33:59 +0200

    Add responsiveness

--- templates/parts/heading-cases.php
+++ templates/parts/heading-cases.php
@@ -1,12 +1,12 @@
 <div class="row page-header">
 	<div class="container pt-6 pb-5">
 		<div class="row justify-content-center">
-			<div class="col-8 text-center">
+			<div class="col-12 col-md-8 text-center">
 				<h1 class="mb-5"><span class="text-orange">Case:</span> <?php the_title(); ?></h1>
 			</div>
 		</div>
 		<div class="row justify-content-center">
-			<div class="col-8">
+			<div class="col-12 col-md-8">
 				<?php the_content(); ?>
 			</div>
 		</div>
--- templates/parts/heading-page.php
+++ templates/parts/heading-page.php
@@ -5,7 +5,7 @@
 	<div class="container py-6">
 	<?php endif;?>
 		<div class="row justify-content-center">
-			<div class="col-8">
+			<div class="col-12 col-md-8">
 				<?php the_content(); ?>
 				<?php if(is_singular('teamleden') && get_field('autograph')):?>
 					<img height="200" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/autographs/<?php the_field('autograph');?>"/>
