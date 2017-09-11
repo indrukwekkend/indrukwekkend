6fcdfb1ebed2869e17b48da28a04690acee6e1c5
odilio <github@odil.io>
2017-09-11 09:54:19 +0200

    Closes: #73

--- templates/parts/about-contact.php
+++ templates/parts/about-contact.php
@@ -1,15 +1,15 @@
 <div id="contact" class="row part-contact about">
 	<div class="container">
 		<div class="row justify-content-center mb-6">
-			<div class="col-10">
+			<div class="col-12 col-lg-10">
 				<div class="row bg-orange justify-content-center">
-						<div class="col-9">
+						<div class="col-12">
 
 							<div class="row mb-4 mt-4 align-items-center">
-								<div class="col-6 text-right">
-									<h4>Hebben we<br>je kunnen<br>overtuigen?</h4>
+								<div class="col-12 col-md-6 text-center text-md-right">
+									<h4>Hebben we<br> je kunnen<br> overtuigen?</h4>
 								</div>
-								<div class="col-6 text-left">
+								<div class="col-12 col-md-6 text-center text-md-left">
 									<h5 class="mb-4">Of kom je eerst langs?</h5>
 									<p>Je kan ons bereiken op <a href="tel:072–5625482">072 – 562 54 82</a></p>
 									<p>Mailen mag ook: <a href="mailto:info@indrukwekkend.nl">info@indrukwekkend.nl</a></p>
@@ -17,27 +17,35 @@
 						</div>
 
 						<div class="row text-center justify-content-center my-3">
+							<div class="col-12 col-lg-11 col-xl-10">
+								<div class="row">
 
-								<div class="col-3 p-0">
-									<span class="icon twitter-white mr-1"></span>
-									<a href="<?php the_field('twitter_link', 'option'); ?>" target="_blank"><?php the_field('twitter_label', 'option'); ?></a>
+									<div class="col-3 col-sm-6  col-xl-3 mb-2 p-0">
+										<a href="<?php the_field('twitter_link', 'option'); ?>" target="_blank">
+											<span class="icon twitter-white mr-1"></span><span class="d-none d-sm-inline"><?php the_field('twitter_label', 'option'); ?></span>
+										</a>
+									</div>
+	
+									<div class="col-3 col-sm-6  col-xl-3 mb-2 p-0">
+										<a href="<?php the_field('facebook_link', 'option'); ?>" target="_blank">
+											<span class="icon facebook-white mr-1"></span><span class="d-none d-sm-inline"><?php the_field('facebook_label', 'option'); ?></span>
+										</a>
+									</div>
+	
+									<div class="col-3 col-sm-6  col-xl-3 mb-2 p-0">
+										<a href="<?php the_field('instagram_link', 'option'); ?>" target="_blank">
+											<span class="icon instagram-white mr-1"></span><span class="d-none d-sm-inline"><?php the_field('instagram_label', 'option'); ?></span>
+										</a>
+									</div>
+	
+									<div class="col-3 col-sm-6 col-xl-3 mb-2 p-0">
+										<a href="<?php the_field('linkedin_link', 'option'); ?>" target="_blank">
+											<span class="icon linkedin-white mr-1"></span><span class="d-none d-sm-inline"><?php the_field('linkedin_label', 'option'); ?></span>
+										</a>
+									</div>
+
 								</div>
-
-								<div class="col-3 p-0">
-									<span class="icon facebook-white mr-1"></span>
-									<a href="<?php the_field('facebook_link', 'option'); ?>" target="_blank"><?php the_field('facebook_label', 'option'); ?></a>
-								</div>
-
-								<div class="col-3 p-0">
-									<span class="icon instagram-white mr-1"></span>
-									<a href="<?php the_field('instagram_link', 'option'); ?>" target="_blank"><?php the_field('instagram_label', 'option'); ?></a>
-								</div>
-
-								<div class="col-3 p-0">
-									<span class="icon linkedin-white mr-1"></span>
-									<a href="<?php the_field('linkedin_link', 'option'); ?>" target="_blank"><?php the_field('linkedin_label', 'option'); ?></a>
-								</div>
-
+							</div>
 						</div>
 					</div>
 
