95787beead659e843428cce3438fc0fbdd33f662
odilio <github@odil.io>
2017-09-11 11:55:14 +0200

    Close #62 and add hover effect to element

--- templates/parts/content-contributors.php
+++ templates/parts/content-contributors.php
@@ -1,7 +1,7 @@
 <div class="row">
-	<div class="col-10">
+	<div class="col-12 col-xl-10">
 		<div class="row align-items-center part-contributors">
-			<div class="col-lg-7">
+			<div class="col-lg-7 order-2 order-lg-1">
 
 				<?php $args = array(
 					'post_type' => 'teamleden',
@@ -21,10 +21,19 @@
 					<?php while( $query->have_posts() ) : ?>
 						<?php $query->the_post(); ?>
 
-						<div class="col contributor">
-							<img class="img-fluid" src="<?php the_post_thumbnail_url('medium');?>" title="<?php the_title();?>"/>
-							
-							<div class="contributor-title"><?php the_title();?></div>
+						<div class="col-12 col-sm contributor">
+							<a class="contributor-content" href="<?php echo esc_url( get_post_permalink() ); ?>">
+								<div class="contributor-image" style="background-image:url(<?php the_post_thumbnail_url();?>);"></div>
+								<div class="contributor-title">
+									<?php the_title(); ?>
+								</div>
+								<div class="contributor-hover">
+									<div class="contributor-info">
+										<h4 class="text-white"><?php the_title(); ?></h4>
+										<span><?php the_field('company_position'); ?></span>
+									</div>
+								</div>
+							</a>
 						</div>
 
 					<?php endwhile; ?>
@@ -34,7 +43,7 @@
 				<?php endif; ?>
 
 			</div>
-			<div class="col-lg-5 text-right">
+			<div class="col-lg-5 order-1 order-xl-2 mb-5 mb-xl-0 text-right">
 
 				<div class="heading">
 					<h4 class="title">
