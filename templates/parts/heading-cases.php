8c2c04bd3f8d55dd686a2134fa15ebbd44ab5306
odilio <github@odil.io>
2017-09-11 13:43:10 +0200

    Add resonsiveness to heading parts

--- templates/parts/heading-cases.php
+++ templates/parts/heading-cases.php
@@ -1,7 +1,7 @@
 <div id="intro" class="row page-header">
 	<div class="container pt-6 pb-5">
 		<div class="row justify-content-center">
-			<div class="col-12 col-md-8 text-center">
+			<div class="col-12 text-center">
 				<h1 class="mb-5"><span class="text-orange">Case:</span> <?php the_title(); ?></h1>
 			</div>
 		</div>
--- templates/parts/heading-right.php
+++ templates/parts/heading-right.php
@@ -1,5 +1,5 @@
 <div class="row justify-content-center pb-5">
-	<div class="col-10">
+	<div class="col-12 col-md-11 col-lg-10">
 		<div class="row">
 			<div class="col-lg-6"></div>
 				<div class="col-lg-5 text-right">
