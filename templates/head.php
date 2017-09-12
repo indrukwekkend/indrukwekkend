f2d46436899a8be7f68c1c8285c0b9d74ca7a0b8
odilio <github@odil.io>
2017-09-12 16:16:28 +0200

    Fix a bug caused by favicon commit

--- templates/head.php
+++ templates/head.php
@@ -2,6 +2,6 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" type="image/x-icon" href="<?= Assets\asset_path('images/favicon.ico') ?>" />
+	<link rel="icon" type="image/x-icon" href="<?= get_template_directory_uri(); ?>/images/favicon.ico" />
 	<?php wp_head(); ?>
 </head>
