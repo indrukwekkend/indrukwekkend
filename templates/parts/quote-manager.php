71a68ae14d5d40c0d7d8e235abaef40a56c722ee
odil-io <github@odil.io>
2017-09-15 10:06:54 +0200

    Fix a small bug
    Removed col-sm-3 as it caused visibility issues on small/medium screen
    resolutions.

--- templates/parts/quote-manager.php
+++ templates/parts/quote-manager.php
@@ -48,17 +48,17 @@ endif;
 
 						<div class="col-8 col-md-12 mb-3 mb-md-4 mb-lg-5">
 							<div class="row">
-								<div class="col-12 col-sm-5 col-md-12 col-lg-5 col-xl-4 mb-4 mb-lg-0 text-nowrap">
+								<div class="col-12 col-md-12 col-lg-5 col-xl-4 mb-4 mb-lg-0 text-nowrap">
 									<span class="icon mail mr-2"></span>
 									<a class="text-white" href="mailto:<?php echo $manager['email'];?>"><?php echo $manager['email'];?></a>
 								</div>
 
-								<div class="col-12 col-sm-3 col-md-6 col-lg-3 mb-4 mb-lg-0 text-nowrap">
+								<div class="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0 text-nowrap">
 									<span class="icon phone mr-2"></span>
 									<a class="text-white" href="tel:<?php echo $manager['landline'];?>"><?php echo $manager['landline'];?></a>
 								</div>
 
-								<div class="col-12 col-sm-3 col-md-6 col-lg-3 mb-4 mb-lg-0 text-nowrap">
+								<div class="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0 text-nowrap">
 									<span class="icon smartphone mr-2"></span>
 									<a class="text-white" href="tel:<?php echo $manager['mobile'];?>"><?php echo $manager['mobile'];?></a>
 								</div>
