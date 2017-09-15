0c5898fd1754b2fc53d49c9b5a12e6168d2e7a57
odil-io <github@odil.io>
2017-09-15 11:02:04 +0200

    Remove acf.php
    It did not work as planned.

--- functions.php
+++ functions.php
@@ -16,7 +16,6 @@ $sage_includes = [
     'lib/titles.php', // Page titles
     'lib/wrapper.php', // Theme wrapper class
     'lib/customizer.php', // Theme customizer
-    'lib/acf.php', // Advanced Custom Fields
 ];
 foreach ( $sage_includes as $file ) {
     if ( ! $filepath = locate_template( $file ) ) {
