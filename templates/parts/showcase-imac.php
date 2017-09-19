cf25c169c09959eb5273cf7d04475c89421a62db
odilio <github@odil.io>
2017-09-19 16:26:42 +0200

    Add beat files for #18

---
+++ templates/parts/showcase-imac.php
@@ -0,0 +1,34 @@
+<div class="row justify-content-center pb-5 part-heading part-showcase-imac">
+	<div class="col-12 col-md-11 col-lg-10">
+		<div class="row">
+			<div class="col-lg-1"></div>
+			<div class="col-lg-5">
+
+				<div class="heading">
+					<h4 class="title">
+						<span><?php echo preg_replace('~((\w+\s){3})~', '$1' . "<br>", "Onderdeel: iMac Video's"); ?></span>
+					</h4>
+					<span class="line"></span>
+				</div>
+
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-6 mt-5 mt-xl-0"></div>
+
+			<div class="col-lg-6">
+				<p>Dit is een test voor de iMac Video optie. De bedoeling is dat men een youtube URL of ID plaatst in een veld en hierna kan zien op de website. Moet wel responsivieus zijn nou.</p>
+			</div>
+
+		</div>
+		<div class="row">
+			<div class="col-12 pt-5 text-center">
+				<div class="wrap">
+					<div class="imac">
+						<iframe class="video" src="https://www.youtube.com/embed/lqU41IuhI3s" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
