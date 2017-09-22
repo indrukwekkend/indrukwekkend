8b1f2cb5241661b948d67c9014d880cc76b81907
odil-io <github@odil.io>
2017-09-22 16:35:02 +0200

    Fix small bug in social icons

--- templates/footer.php
+++ templates/footer.php
@@ -3,12 +3,12 @@
 		<div class="container">
 			<div class="row justify-content-center pb-5 py-xl-5">
 
-				<div class="col-sm-8 col-xl-3 col-md-6 mt-5 mt-xl-0 contact">
+				<div class="col-sm-8 col-md-6 col-xl-3 mt-5 mt-xl-0 contact">
 					<div class="row align-items-center">
 						<div class="col-2">
 							<div class="icon call-us-gray"></div>
 						</div>
-						<div class="col-10">
+						<div class="col-10 pl-3">
 							<div class="row">
 								<div class="col-12"><p>De beller is sneller <strong class="d-inline d-md-none"><a class="text-gray" href="tel:072-5625482">072 - 562 54 82</a></strong></p></div>
 							</div>
@@ -19,12 +19,12 @@
 					</div>
 				</div>
 
-				<div class="col-sm-8 col-xl-3 col-md-6 mt-5 mt-xl-0 contact">
+				<div class="col-sm-8 col-md-6 col-xl-3 mt-5 mt-xl-0 contact">
 					<div class="row align-items-center">
 						<div class="col-2">
 							<div class="icon mail-us-gray"></div>
 						</div>
-						<div class="col-10">
+						<div class="col-10 pl-3">
 							<div class="row">
 								<div class="col-12"><p>Mailen mag ook <strong class="d-inline d-md-none"><a class="text-gray" href="mailto:info@indrukwekkend.nl">info@indrukwekkend.nl</a></strong></p></div>
 							</div>
@@ -35,12 +35,12 @@
 					</div>
 				</div>
 
-				<div class="col-sm-8 col-xl-3 col-md-6 mt-5 mt-xl-0 contact">
+				<div class="col-sm-8 col-md-6 col-xl-3 mt-5 mt-xl-0 contact">
 					<div class="row align-items-center">
 						<div class="col-2">
 							<div class="icon visit-us-gray"></div>
 						</div>
-						<div class="col-10">
+						<div class="col-10 pl-3">
 							<div class="row">
 								<div class="col-12"><p>Hier vind je ons <strong class="d-inline d-md-none"><a class="text-gray" href="https://www.google.com/maps/dir/Current+Location/De Droogmakerij+20f+1851+LX+Heiloo" target="_blank">De Droogmakerij 20f, 1851 LX Heiloo</a></strong></p></div>
 							</div>
@@ -51,14 +51,14 @@
 					</div>
 				</div>
 
-				<div class="col-sm-8 col-xl-3 col-md-6 mt-5 mt-xl-0">
+				<div class="col-sm-8 col-md-6 col-xl-3 mt-5 mt-xl-0">
 					<div class="row align-items-center">
 						<div class="col text-center text-md-left text-xl-right">
-							<span class="pr-3 pr-md-2">Wij zijn sociaal</span>
-							<a class="pr-3 pr-md-2" href="<?php the_field('instagram_link', 'option'); ?>" target="_blank"><span class="icon instagram-gray"></span></a>
-							<a class="pr-3 pr-md-2" href="<?php the_field('facebook_link', 'option'); ?>" target="_blank"><span class="icon facebook-gray"></span></a>
-							<a class="pr-3 pr-md-2" href="<?php the_field('twitter_link', 'option'); ?>" target="_blank"><span class="icon twitter-gray"></span></a>
-							<a class="pr-3 pr-md-2" href="<?php the_field('linkedin_link', 'option'); ?>" target="_blank"><span class="icon linkedin-gray"></span></a>
+							<span class="pr-md-2">Wij zijn sociaal</span>
+							<a class="pr-lg-2" href="<?php the_field('instagram_link', 'option'); ?>" target="_blank"><span class="icon instagram-gray"></span></a>
+							<a class="pr-lg-2" href="<?php the_field('facebook_link', 'option'); ?>" target="_blank"><span class="icon facebook-gray"></span></a>
+							<a class="pr-lg-2" href="<?php the_field('twitter_link', 'option'); ?>" target="_blank"><span class="icon twitter-gray"></span></a>
+							<a class="" href="<?php the_field('linkedin_link', 'option'); ?>" target="_blank"><span class="icon linkedin-gray"></span></a>
 						</div>
 					</div>
 				</div>
