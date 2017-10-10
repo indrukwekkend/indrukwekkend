27ab5d9c66ea1efeface69faff0e568cf11322b8
odilio <github@odil.io>
2017-10-10 12:39:50 +0200

    Fix indenting

--- templates/header.php
+++ templates/header.php
@@ -1,11 +1,11 @@
 <?php
 $background = function ( $url ) {
-    $url = ($url) ? $url : get_stylesheet_directory_uri() . "/dist/images/header_default.jpg";
-    echo 'style="background-image: url(' . esc_url( $url ) . ');"';
+	$url = ($url) ? $url : get_stylesheet_directory_uri() . "/dist/images/header_default.jpg";
+	echo 'style="background-image: url(' . esc_url( $url ) . ');"';
 };
 
 $display = function ( $url ) {
-    if ($url != ''){echo 'd-none';}
+	if ($url != ''){echo 'd-none';}
 };
 
 ?>
@@ -64,49 +64,37 @@ $display = function ( $url ) {
 					<div class="headlines">
 						<span id="headlines-typed"></span>
 					</div>
-					
-					
 
-							<div class="buttons mb-5 mt-4 mb-xl-6">
-								
-								<?php if ( is_singular( array( 'teamleden', 'cases' ) ) ) : ?>
-	
-								    <?php if (is_singular( 'teamleden' )) {?>
-									   	 <a class="btn btn-lg btn-primary btn-arrow arrow-down mr-2" href="#intro">
-											Leer <?php the_title( ) ?> Kennen
-										</a>
-								    <?php }else{ ?>
-									    	<a class="btn btn-lg btn-primary btn-arrow arrow-down mr-2" href="#intro">
-												Lees onze aanpak
-											</a>
-								    <?php } ?>
-								    
-								<?php elseif (is_page( 'over-ons' )): ?>
-								
+					<div class="buttons mb-5 mt-4 mb-xl-6">
+
+						<?php if ( is_singular( array( 'teamleden', 'cases' ) ) ) : ?>
+
+							<?php if (is_singular( 'teamleden' )) {?>
+								<a class="btn btn-lg btn-primary btn-arrow arrow-down mr-2" href="#intro">
+									Leer <?php the_title( ) ?> Kennen
+								</a>
+							<?php }else{ ?>
 									<a class="btn btn-lg btn-primary btn-arrow arrow-down mr-2" href="#intro">
-										Leer ons beter kennen</a>
-								
-								<?php elseif (is_page( 'contact' )): ?>
-								
-									<a class="btn btn-lg btn-primary btn-arrow arrow-right mr-2" href="https://www.google.com/maps/dir/Current+Location/Indrukwekkend,+De+Droogmakerij+20f,+1851+LX+Heiloo" target="_blank">
-										Check de route</a>
-								
-								<?php elseif (is_home() || is_front_page(  )): ?>
-								
-									<a class="btn btn-lg btn-primary btn-arrow arrow-right mr-2" href="/over-ons">
-										Leer ons kennen	</a>
-									<a class="btn btn-lg btn-black" href="#cases">
-										Bekijk ons werk</a>
-										
-								<?php endif;?>
-	
+									Lees onze aanpak
+									</a>
+							<?php } ?>
 
+							<?php elseif (is_page( 'over-ons' )): ?>
+								<a class="btn btn-lg btn-primary btn-arrow arrow-down mr-2" href="#intro">
+									Leer ons beter kennen</a>
+							
+							<?php elseif (is_page( 'contact' )): ?>
+								<a class="btn btn-lg btn-primary btn-arrow arrow-right mr-2" href="https://www.google.com/maps/dir/Current+Location/Indrukwekkend,+De+Droogmakerij+20f,+1851+LX+Heiloo" target="_blank">
+									Check de route</a>
+							
+							<?php elseif (is_home() || is_front_page(  )): ?>
+								<a class="btn btn-lg btn-primary btn-arrow arrow-right mr-2" href="/over-ons">
+									Leer ons kennen	</a>
+								<a class="btn btn-lg btn-black" href="#cases">
+									Bekijk ons werk</a>
 
-
-							</div>
-
-						
-
+							<?php endif;?>
+						</div>
 					</div>
 
 					<?php if($image = get_field('header_foreground') ): ?>
@@ -124,4 +112,5 @@ $display = function ( $url ) {
 			</div>
 		</div>
 	<?php endif; ?>
+
 </header>
