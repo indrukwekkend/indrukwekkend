5c99054f05d6911b9ee616225a4cf575fc506391
hphioolen <hioolen@mac.com>
2017-11-09 11:28:34 +0100

    Quotes structuur
    Quote structuur aangepast: direct onder content

--- templates/parts/heading-right.php
+++ templates/parts/heading-right.php
@@ -33,11 +33,33 @@
 
 			<div class="col-lg-6 mt-5 mt-xl-0 ">
 				<?php the_sub_field('content'); ?>
+				
+				
+				<?php $extra = get_sub_field('extra_content'); ?>
+				
+				<?php if (get_sub_field('extra_content_boolean') && !$extra['afbeelding_over_volle_breedte'] && $extra['quote_boolean']) : ?>
+	
+					<div class="part-quote">						
+						<div class="pt-4 px-4 pb-3 pt-md-5 px-md-5 pb-md-4 mb-5 quote-container">
+															
+							<div class="quote-content">
+								<?= $extra['quote'] ?>
+							</div>
+							<div class="quote-autograph">
+								<img height="120" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/autographs/<?php the_field('autograph',  $extra['author'] );?>"/>	
+							</div>
+							
+						</div>
+					</div>
+					
+				<?php endif; //Quote ?>
+				
+				
 			</div>
 
 			<?php if (get_sub_field('extra_content_boolean')) : ?>
 			
-				<?php $extra = get_sub_field('extra_content'); ?>
+				
 			
 				<?php if ( $extra['afbeelding_over_volle_breedte']): 
 					
@@ -78,21 +100,4 @@
 		</div>
 	</div>
 </div>
-
-<?php if (get_sub_field('extra_content_boolean') && !$extra['afbeelding_over_volle_breedte'] && $extra['quote_boolean']) : ?>
-	
-	<div class="row part-quote <?=$reverser?>">
-		<div class="col-lg-5 p-0">
-			<div class="pt-4 px-4 pb-3 pt-md-5 px-md-5 pb-md-4 mb-5 quote-container">
-				<div class="quote-content">
-					<?= $extra['quote'] ?>
-				</div>
-				<div class="quote-autograph">
-					<img height="120" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/autographs/<?php the_field('autograph',  $extra['author'] );?>"/>
-				</div>
-			</div>
-		</div>
-	</div>
-	
-<?php endif; //Quote ?>
 <!-- Einde Titel / Heading right -->
