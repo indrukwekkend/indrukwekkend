9b9faac8e0f1d87b7218a7e51bf73a30ca1a9e8e
odilio <github@odil.io>
2017-11-14 17:16:19 +0100

    Titel veld toegevoegd aan Client Quote

--- templates/parts/quote-client.php
+++ templates/parts/quote-client.php
@@ -6,22 +6,39 @@
 	<?php setup_postdata( $post ); ?>
 
 	<div class="row pt-6 part-quote-client">
-		<div class="col-12 col-xl-10">
+
+		<div class="col-12 col-xl-8">
+
 			<div class="row p-5 quote-client-container">
+
 				<div class="col-12">
+
 					<div class="row">
 						<div class="col-12">
 							<?php the_content();?>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-12">
-							<span class="text-gray"><?php the_title(); ?> - <?php the_field('company_position');?></span>
-						</div>
-					</div>
+
 				</div>
+
 			</div>
+
 		</div>
+
+		<div class="col-12 col-xl-4 text-right">
+
+			<div class="heading">
+
+				<h4 class="title">
+					<span><?php the_field('quote_title'); ?></span>
+				</h4>
+
+				<span class="line"></span>
+
+			</div>
+
+		</div>
+
 	</div>
 
 	<?php wp_reset_postdata();?>
