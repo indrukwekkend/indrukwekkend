19491e1b5abc43613be4046342bf036419f2e6e0
odil-io <github@odil.io>
2017-11-17 16:02:33 +0100

    Fix: zichtbaren iconen zonder inhoud
    Indien het telefoon nummer en/of email adres niet is ingevuld worden de
    iconen verborgen.

--- templates/parts/overlay-teamleden.php
+++ templates/parts/overlay-teamleden.php
@@ -19,17 +19,22 @@
 								<div class="overlay-teamlid-title">
 									<h4><?php the_title(); ?></h4>
 									
-									<div class="overlay-teamlid-contact">
-												
-										<a class="ml-1 text-black" href="tel:<?php the_field('mobile'); ?>">
-											<span class="icon smartphone"></span>
-										</a>
-										<a class="ml-1 text-orange" href="mailto:<?php the_field('email'); ?>">
-											<span class="icon mail"></span>
-										</a>
-												
-									</div>
+									<div class="overlay-teamlid-contact d-md-none">
 									
+										<?php if( get_field('mobile') ) : ?>
+											<a class="ml-1 text-black" href="tel:<?php the_field('mobile'); ?>">
+												<span class="icon smartphone"></span>
+											</a>
+										<?php endif; ?>
+										
+										<?php if( get_field('email') ) : ?>
+											<a class="ml-1 text-orange" href="mailto:<?php the_field('email'); ?>">
+												<span class="icon mail"></span>
+											</a>
+										<?php endif; ?>
+
+									</div>
+
 								</div>
 								<div class="overlay-teamlid-hover">
 									<a class="click-area" href="<?php echo esc_url( get_post_permalink() ); ?>"></a>
@@ -38,18 +43,24 @@
 										<span class="d-none d-sm-inline" ><?php the_field('company_position'); ?></span>
 									</div>
 									<div class="overlay-teamlid-contact">
+									
+									<?php if( get_field('mobile') ) : ?>
 										<div class="overlay-teamlid-phone">
 											<div class="mb-1 ">
 												<span class="icon smartphone"></span>
 												<a class="ml-1 text-white" href="tel:<?php the_field('mobile'); ?>"><?php the_field('mobile'); ?></a>
 											</div>
 										</div>
+									<?php endif; ?>
+
+									<?php if( get_field('email') ) : ?>
 										<div class="overlay-teamlid-email">
 											<div class="mb-2 ">
 												<span class="icon mail"></span>
 												<a class="ml-1 text-white" href="mailto:<?php the_field('email'); ?>"><?php the_field('email'); ?></a>
 											</div>
 										</div>
+									<?php endif; ?>
 									</div>
 								</div>
 							</div>
