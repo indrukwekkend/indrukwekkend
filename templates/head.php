4f978d8167f476492c980c009b540f0065aa073c
odil-io <github@odil.io>
2017-11-23 10:52:55 +0100

    Analytics toegevoegd

--- templates/head.php
+++ templates/head.php
@@ -3,5 +3,7 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" type="image/x-icon" href="<?= get_template_directory_uri(); ?>/dist/images/favicon.png" />
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-30021780-46"></script>
+	<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-30021780-46');</script>
 	<?php wp_head(); ?>
 </head>
