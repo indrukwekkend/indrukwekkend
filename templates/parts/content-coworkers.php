dbcedc1d41bf1da58b27bfad831cfc4633a2562c
odil-io <github@odil.io>
2017-11-23 11:13:24 +0100

    Fix: iconen coworkers
    Alleen zichtbaar wanneer ingevuld.

--- templates/parts/content-coworkers.php
+++ templates/parts/content-coworkers.php
@@ -34,18 +34,25 @@ $coworkers_title = (is_page_template('template-about.php')) ? "Maak kennis met h
 										<span class="d-none d-sm-inline"><?php the_field('company_position'); ?></span>
 									</div>
 									<div class="coworkers-contact">
+
+										<?php if(get_field('mobile')):?>
 										<div class="coworkers-phone">
 											<div class="mb-2 px-4">
 												<span class="icon smartphone d-none d-sm-inline-block"></span>
 												<a class="ml-2 text-white" href="tel:<?php the_field('mobile'); ?>"><?php the_field('mobile'); ?></a>
 											</div>
 										</div>
+										<?php endif; ?>
+
+										<?php if(get_field('mobile')):?>
 										<div class="coworkers-email">
 											<div class="mb-2 px-4">
 												<span class="icon mail d-none d-sm-inline-block"></span>
 												<a class="ml-2 text-white" href="mailto:<?php the_field('email'); ?>"><?php the_field('email'); ?></a>
 											</div>
 										</div>
+										<?php endif; ?>
+
 									</div>
 								</div>
 							</div>
