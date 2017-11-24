4c5a2f5e10910536e0f0eb33ac3c0fff0b6e954d
odil-io <github@odil.io>
2017-11-24 13:00:31 +0100

    Tekstuele wijzigingen

--- templates/parts/about-contact.php
+++ templates/parts/about-contact.php
@@ -7,10 +7,10 @@
 
 							<div class="row mb-4 mt-4 align-items-center">
 								<div class="col-12 col-md-6 text-center text-md-right border-right">
-									<h4>Hebben we<br> je kunnen<br> overtuigen?</h4>
+									<h4>Toe aan een<br> beetje energie?</h4>
 								</div>
 								<div class="col-12 col-md-6 text-center text-md-left">
-									<h5 class="mb-4">Of kom je eerst langs?</h5>
+									<h5 class="mb-4">Wij geven het graag!</h5>
 									<p>Je kan ons bereiken op <a href="tel:072–5625482" class="text-nowrap">072 – 562 54 82</a></p>
 									<p>Mailen mag ook: <a href="mailto:info@indrukwekkend.nl">info@indrukwekkend.nl</a></p>
 							</div>
@@ -25,19 +25,19 @@
 											<span class="icon twitter-white mr-1"></span><span class="d-none d-sm-inline"><?php the_field('twitter_label', 'option'); ?></span>
 										</a>
 									</div>
-	
+
 									<div class="col-3 col-sm-6  col-xl-3 mb-2 p-0">
 										<a href="<?php the_field('facebook_link', 'option'); ?>" target="_blank">
 											<span class="icon facebook-white mr-1"></span><span class="d-none d-sm-inline"><?php the_field('facebook_label', 'option'); ?></span>
 										</a>
 									</div>
-	
+
 									<div class="col-3 col-sm-6  col-xl-3 mb-2 p-0">
 										<a href="<?php the_field('instagram_link', 'option'); ?>" target="_blank">
 											<span class="icon instagram-white mr-1"></span><span class="d-none d-sm-inline"><?php the_field('instagram_label', 'option'); ?></span>
 										</a>
 									</div>
-	
+
 									<div class="col-3 col-sm-6 col-xl-3 mb-2 p-0">
 										<a href="<?php the_field('linkedin_link', 'option'); ?>" target="_blank">
 											<span class="icon linkedin-white mr-1"></span><span class="d-none d-sm-inline"><?php the_field('linkedin_label', 'option'); ?></span>
